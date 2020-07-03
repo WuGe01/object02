80eeaeeb39c1008c04ca9d4959e0cb34d7ae2638
吳宗融 <bc781208@hotmail.com>
2020-07-03 15:24:54 +0800

    最新人文章簡易做法

--- api/good.php
+++ api/good.php
@@ -3,13 +3,17 @@ include_once "../css/base2.php";
 $id=$_POST['id'];
 $type=$_POST['type'];
 $user=$_POST['user'];
-
+$post=$news->find($id);
 switch ($type) {
     case '1':
         $log->save(['news'=>$id,'user'=>$user]);
+        $post['good']++;
+        $news->save($post);
         break;
     case '2':
         $log->del(['news'=>$id,'user'=>$user]);
+        $post['good']--;
+        $news->save($post);
         break;
 
 }
